60fa6b7cf6bf4031088d7a3aa1a1d8d56dec2a47
ASM@GitHub2024 <asmaa14has@gmail.com>
2025-12-02 07:03:16 +0200

    Form Validation

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,5 @@
-import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
-
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { getTodoAction } from "@/actions/todo.actions";
+import AddTodoForm from "@/components/AddTodoForm";
 
 export default async function Home() {
 
@@ -21,51 +7,11 @@ const todos = await getTodoAction();
   return (
     <main>
       
-      <ul>
+      {/* <ul>
         {todos.map(ele => <li key={ele.id}>{ele.title}</li>)}
-      </ul>
+      </ul> */}
 
-      {/* <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <Dialog>
-        <form>
-          <DialogTrigger asChild>
-            <Button variant={"outline"}>
-              <Plus className="mr-1" size={14} />
-              New Todo
-            </Button>
-          </DialogTrigger>
-          <DialogContent className="sm:max-w-[425px]">
-            <DialogHeader>
-              <DialogTitle>Edit profile</DialogTitle>
-              <DialogDescription>
-                Make changes to your profile here. Click save when you&apos;re
-                done.
-              </DialogDescription>
-            </DialogHeader>
-            <div className="grid gap-4">
-              <div className="grid gap-3">
-                <Label htmlFor="name-1">Name</Label>
-                <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
-              </div>
-              <div className="grid gap-3">
-                <Label htmlFor="username-1">Username</Label>
-                <Input
-                  id="username-1"
-                  name="username"
-                  defaultValue="@peduarte"
-                />
-              </div>
-            </div>
-            <DialogFooter>
-              <DialogClose asChild>
-                <Button variant="outline">Cancel</Button>
-              </DialogClose>
-              <Button type="submit">Save changes</Button>
-            </DialogFooter>
-          </DialogContent>
-        </form>
-      </Dialog>
-    </div> */}
+      <AddTodoForm />
     </main>
   );
 }
---
+++ components/AddTodoForm.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Plus } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { Button } from "./ui/button";
+import { Textarea } from "./ui/textarea";
+import { todoFormSchema, TodoFormValues } from "@/shema";
+
+const AddTodoForm = () => {
+  const defaultValues: Partial<TodoFormValues> = {
+    title: "DEFAULT TITLE",
+    body: "DEFAULT BODY",
+  };
+
+  // const todos = await getTodoListAction();
+  const form = useForm<TodoFormValues>({
+    resolver: zodResolver(todoFormSchema),
+    defaultValues,
+    mode: "onChange",
+  });
+
+  const onSubmit = (data:TodoFormValues) => {
+    console.log(data)
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>
+          <Plus size={14} className="mr-1" />
+          New Todo
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>Make changes to your profile here. Click save when you&apos;re done.</DialogDescription>
+        </DialogHeader>
+        <div className="py-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="title"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Title</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Got to gym" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="body"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Short Description</FormLabel>
+                    <FormControl>
+                      <Textarea placeholder="Tell us a little bit about yourself" className="resize-none" {...field} />
+                    </FormControl>
+                    <FormDescription>You can write a short description about your next todo.</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+          <Button type="submit">Save changes</Button>
+            </form>
+          </Form>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default AddTodoForm;
