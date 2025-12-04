9ee9d2e0d1173f755e9a89e1cd8ef30644173b96
ASM@GitHub2024 <asmaa14has@gmail.com>
2025-12-04 08:22:57 +0200

    fix prisma seed and package.json for deployment on vercel

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,40 +1,40 @@
-import { PrismaClient } from '../generated/prisma/client'
-import {faker} from '@faker-js/faker'
+ import { PrismaClient } from '../generated/prisma/client'
+// import {faker} from '@faker-js/faker'
 
-const prisma = new PrismaClient()
-async function main() {
+// const prisma = new PrismaClient()
+// async function main() {
 
-  // Generate fale data for todo moel 
-   await prisma.todo.createMany({
-        data:Array.from({length:20}, () => ({
-              title:faker.lorem.words({min:2,max:5}),
-              body:faker.lorem.words({min:1,max:10})
-            }))
-    })
+//   // Generate fale data for todo moel 
+//   //  await prisma.todo.createMany({
+//   //       data:Array.from({length:20}, () => ({
+//   //             title:faker.lorem.words({min:2,max:5}),
+//   //             body:faker.lorem.words({min:1,max:10})
+//   //           }))
+//   //   })
 
-  // Generate Fake data for User model
-    // await prisma.user.createMany({
-    //     data:Array.from({length:20}, () => (
-    //         {
-    //         name:faker.internet.username(),
-    //         email:faker.internet.email(),
-    //         address:{
-    //           street:faker.location.street(),
-    //           state:faker.location.state(),
-    //           city:faker.location.city(),
-    //           zipCode:faker.location.zipCode()
-    //         }
-    //     }
-    //     ))
-    // })
-}
+//   // Generate Fake data for User model
+//     // await prisma.user.createMany({
+//     //     data:Array.from({length:20}, () => (
+//     //         {
+//     //         name:faker.internet.username(),
+//     //         email:faker.internet.email(),
+//     //         address:{
+//     //           street:faker.location.street(),
+//     //           state:faker.location.state(),
+//     //           city:faker.location.city(),
+//     //           zipCode:faker.location.zipCode()
+//     //         }
+//     //     }
+//     //     ))
+//     // })
+// }
 
-main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+// main()
+//   .then(async () => {
+//     await prisma.$disconnect()
+//   })
+//   .catch(async (e) => {
+//     console.error(e)
+//     await prisma.$disconnect()
+//     process.exit(1)
+//   })
